a159fbdcb1b7c34836eb54ed2858736ed28ae049
Ron Craig <ron.craig@comcast.net>
2018-02-23 13:10:11 -0800

    fail project logic .all spec (returns all projects)

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -36,13 +36,13 @@ describe Project do
       expect(Project.all).to eq []
     end
 
-    # it 'returns all projects' do
-    #   project1 = Project.new({:title => 'Teaching Kids to Code', :name => nil, :project_id => 8, :id => nil})
-    #   project1.save
-    #   project2 = Project.new({:title => 'Teaching Ruby to Kids', :name => nil, :project_id => 11, :id => nil})
-    #   project2.save
-    #   expect(Project.all).to eq [project1, project2]
-    # end
+    it 'returns all projects' do
+      project1 = Project.new({:title => 'Teaching Kids to Code', :name => nil, :project_id => 8, :id => nil})
+      project1.save
+      project2 = Project.new({:title => 'Teaching Ruby to Kids', :project_id => 11, :id => nil})
+      project2.save
+      expect(Project.all).to eq [project1, project2]
+    end
   end
 
   describe '#save' do
