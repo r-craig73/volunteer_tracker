9ecf982c6c37c551ad94d3bcb8d4c0476506d4b5
Ron Craig <ron.craig@comcast.net>
2018-02-23 11:37:07 -0800

    edit files and 6 logic specs are failing, 3 projects, 3 volunteers

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
-require "sinatra"
-require "sinatra/reloader"
-require "./lib/project"
-require "./lib/volunteer"
-also_require "lib/**/*.rb"
-require "pg"
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/project')
+require('./lib/volunteer')
+#also_require('lib/**/*.rb')
+require('pg')
 
 DB = PG.connect({:dbname => "volunteer_tracker"})
