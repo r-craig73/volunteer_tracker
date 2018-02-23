2ab59578abfaa85108c36029304b8ca4e05277b5
Ron Craig <ron.craig@comcast.net>
2018-02-23 12:53:00 -0800

    pass one .all project: empty array

--- lib/project.rb
+++ lib/project.rb
@@ -15,8 +15,16 @@ attr_accessor(:name)
   end
 
   def self.all
-
-
+    returned_projects = DB.exec("SELECT * FROM projects;")
+    projects = []
+    returned_projects.each() do |item|
+      id = item.fetch("id").to_i()
+      title = item.fetch("title")
+      project_id = item.fetch("project_id")
+      name = item.fetch("name")
+      projects.push(Project.new({:title => title, :project_id => project_id, :name => name, :id => id}))
+    end
+    projects
   end
 
 
