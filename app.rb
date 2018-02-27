f0fae512822ef5000fe5710442fa2c9ab43755c0
Ron Craig <ron.craig@comcast.net>
2018-02-26 23:26:08 -0800

    fail a front end spec, user input a project and view the saved project

--- app.rb
+++ app.rb
@@ -4,3 +4,18 @@ require('./lib/project')
 require('./lib/volunteer')
 also_reload('lib/**/*.rb')
 require('pg')
+require('pry')
+
+DB = PG.connect({:dbname => 'volunteer_tracker'})
+
+get('/') do
+  erb(:homepage)
+end
+
+# post('/projects') do
+#   title = params.fetch('title')
+#   project = Project.new({:title => title, :id => nil})
+#   project.save()
+#   @projects = Project.all()
+#   erb(:success)
+# end
